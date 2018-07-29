35b192d4cefe387433c50e2fd43b3b379c6bef17
Luca Ban - Black Birdy <mesqueeb@gmail.com>
2018-07-29 13:19:45 +0900

    deepmerge tests 🌮

--- src/utils/deepmerge.js
+++ src/utils/deepmerge.js
@@ -1,8 +1,11 @@
 import { isObject } from 'is-what'
-import deepmerge from 'nanomerge'
-// import deepmerge from './nanomerge'
+import nanomerge from 'nanomerge'
+// import deepmerge from 'deepmerge'
+// import deepAssign from 'deep-object-assign-with-reduce'
+// const mergeOptions = require('merge-options')
 
 function merge (...params) {
+  // check if all are objects
   let l = params.length
   for (l; l > 0; l--) {
     const item = params[l - 1]
@@ -11,7 +14,16 @@ function merge (...params) {
       return item
     }
   }
-  return deepmerge(...params)
+  return nanomerge(...params)
+  // settings for 'deepmerge'
+  // const overwriteMerge = (destinationArray, sourceArray, options) => sourceArray
+  // const options = {arrayMerge: overwriteMerge}
+  // if (params.length > 2) {
+  //   return deepmerge.all([...params], options)
+  // }
+  // return deepmerge(...params, options)
+  // return deepAssign(...params)
+  // return mergeOptions(...params)
 }
 
 export default merge
