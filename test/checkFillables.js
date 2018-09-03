64382bcd9f39e0b4794cf938732455b72455d405
Luca Ban - Black Birdy <mesqueeb@gmail.com>
2018-09-04 00:15:39 +0900

    critical bug fixes for checkFillables 🐰

---
+++ test/checkFillables.js
@@ -0,0 +1,16 @@
+import checkFillables from './helpers/checkFillables.cjs'
+import test from 'ava'
+
+test('checkFillables', t => {
+  let res, doc, fillables, guard
+  doc = {name: 'n1', id: '1', filled: true, notfilled: false}
+  fillables = ['name', 'filled', 'id']
+  res = checkFillables(doc, fillables, guard)
+  t.deepEqual(res, {name: 'n1', id: '1', filled: true})
+
+  doc = {name: 'n1', id: '1', filled: true, guarded: true}
+  fillables = []
+  guard = ['guarded']
+  res = checkFillables(doc, fillables, guard)
+  t.deepEqual(res, {name: 'n1', id: '1', filled: true})
+})
--- test/helpers/checkFillables.cjs.js
+++ test/helpers/checkFillables.cjs.js
@@ -1,4 +1,8 @@
-import { isObject } from 'is-what'
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var isWhat = require('is-what');
 
 /**
  * Checks all props of an object and deletes guarded and non-fillables.
@@ -9,20 +13,23 @@ import { isObject } from 'is-what'
  *
  * @returns {object} the cleaned object after deleting guard and non-fillables
  */
-export default function (obj, fillables = [], guard = []) {
-  if (!isObject(obj)) return obj
-  return Object.keys(obj).reduce((carry, key) => {
+function checkFillables (obj) {
+  var fillables = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+  var guard = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+
+  if (!isWhat.isObject(obj)) return obj;
+  return Object.keys(obj).reduce(function (carry, key) {
     // check fillables
     if (fillables.length && !fillables.includes(key)) {
-      return carry
+      return carry;
     }
     // check guard
-    guard.push('_conf')
-    guard.push('_sync')
     if (guard.includes(key)) {
-      return carry
+      return carry;
     }
-    carry[key] = obj[key]
-    return carry
-  }, {})
+    carry[key] = obj[key];
+    return carry;
+  }, {});
 }
+
+exports.default = checkFillables;
