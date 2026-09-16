90604bf77666f7d7a9a29cec28806495bf667f17
Luca Ban <mesqueeb@users.noreply.github.com>
2026-09-17 05:48:41 +1200

    chore: deploy docs from HEAD instead of hardcoded master ref

--- docs/.vuepress/deploy.sh
+++ docs/.vuepress/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mesqueeb/vuex-easy-firestore.git master:gh-pages
+git push -f git@github.com:mesqueeb/vuex-easy-firestore.git HEAD:gh-pages
 
 cd -
